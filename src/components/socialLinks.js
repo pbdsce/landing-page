5f544653afc6b3c1fb4ba020017873a9e9fd2319
Sparsh Kesari <39815871+SparshKesari@users.noreply.github.com>
2020-11-20 22:02:34 +0530

    sidebar removed

--- src/components/socialLinks.js
+++ src/components/socialLinks.js
@@ -18,18 +18,9 @@ const data = [
     icon: <FaLinkedin className="social-icon"></FaLinkedin>,
     // url: "https://www.twitter.com",
   },
-    // {
-    //   id: 3,
-    //   icon: <FaDribbbleSquare className="social-icon"></FaDribbbleSquare>,
-    //   // url: "https://www.twitter.com",
-    // },
-    // {
-    //   id: 4,
-    //   icon: <FaBehanceSquare className="social-icon"></FaBehanceSquare>,
-    //   // url: "https://www.twitter.com",
-    // },
+   
   {
-    id: 5,
+    id: 3,
     icon: <FaTwitterSquare className="social-icon"></FaTwitterSquare>,
     // url: "https://www.twitter.com",
   },
