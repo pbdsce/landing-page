17168609d0ab076a3ca248fc6b4956553b669ace
TheGreatFellow <shreyas42000@gmail.com>
2021-06-30 11:32:05 +0530

    added new Carousels and links

--- src/components/layout/partials/FooterNav.js
+++ src/components/layout/partials/FooterNav.js
@@ -1,38 +1,34 @@
-import React from 'react';
-import classNames from 'classnames';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import classNames from 'classnames'
+import { Link } from 'react-router-dom'
 
-const FooterNav = ({
-  className,
-  ...props
-}) => {
+const FooterNav = ({ className, ...props }) => {
+    const classes = classNames('footer-nav', className)
 
-  const classes = classNames(
-    'footer-nav',
-    className
-  );
-
-  return (
-    <nav
-      {...props}
-      className={classes}
-    >
-      <ul className="list-reset">
-        <li>
-          <Link to="#0">Contact</Link>
-        </li>
-        <li>
-          <Link to="#0">About us</Link>
-        </li>
-        <li>
-          <Link to="#0">Teams</Link>
-        </li>
-        <li>
-          <Link to="#0"><a href={`mailto:support@pointblank.club`} target="_blank">Support</a></Link>
-        </li>
-      </ul>
-    </nav>
-  );
+    return (
+        <nav {...props} className={classes}>
+            <ul className='list-reset'>
+                <li>
+                    <a href='https://blog.pointblank.club/' target='_blank'>
+                        Blog
+                    </a>
+                </li>
+                <li>
+                    <a href='https://forum.dsce.in/' target='_blank'>
+                        Forum
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href='https://forms.gle/yCwMHcn2iatnk9mW6'
+                        target='_blank'
+                    >
+                        Contact Us
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    )
 }
 
-export default FooterNav;
+export default FooterNav
