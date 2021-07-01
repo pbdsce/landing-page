1f0d3a45620de786023775f1d28021c817d814b3
Sparsh Kesari <39815871+SparshKesari@users.noreply.github.com>
2021-07-02 00:08:34 +0530

    rearranged banners and twitter
    Signed-off-by: Sparsh Kesari <39815871+SparshKesari@users.noreply.github.com>

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,28 +1,36 @@
-import React from 'react';
+import React from 'react'
 // import sections
-import Hero from '../components/sections/Hero';
-import FeaturesTiles from '../components/sections/FeaturesTiles';
-import FeaturesSplit from '../components/sections/FeaturesSplit';
+import Hero from '../components/sections/Hero'
+import FeaturesTiles from '../components/sections/FeaturesTiles'
+import FeaturesSplit from '../components/sections/FeaturesSplit'
 // import Testimonial from '../components/sections/Testimonial';
-import Cta from '../components/sections/Cta';
+import Cta from '../components/sections/Cta'
 // import Events from '../components/sections/Events';
 
-
 const Home = () => {
-
   return (
     <>
-
-      <Hero className="illustration-section-01" />
-      <a name="ffteams"></a>
-      <FeaturesTiles />
-    
-      {/* <Events /> */}
+      <Hero className='illustration-section-01' />
+      <a name='ffteams'></a>
       <Cta split />
-      <FeaturesSplit invertMobile topDivider imageFill className="illustration-section-02" />
+      <br></br>
+      <br></br>
+
+      <br></br>
+
+      <FeaturesTiles />
+
+      {/* <Events /> */}
+      {/* <Cta split /> */}
+      <FeaturesSplit
+        invertMobile
+        topDivider
+        imageFill
+        className='illustration-section-02'
+      />
       <Cta split />
     </>
-  );
+  )
 }
 
-export default Home;
+export default Home
